5597fb28431166dc174b1916184f736c938e7b3a
kianga722 <kianga722@gmail.com>
2019-04-20 11:27:18 -0400

    Added mobile response

--- index.js
+++ index.js
@@ -33,9 +33,22 @@ const imgSites = [
   'https://unsplash.it',
 ];
 
+// Random sentences
+const sentences = [
+  'Haul wind cackle fruit spike lass quarterdeck bring a spring upon her cable bilge take a caulk Pieces of Eight loaded to the gunwalls Shiver me timbers ho scuttle pink nipperkin hearties port.',
+  'Hempen halter hogshead barkadeer parrel gaff take a caulk plunder Jack Tar log Jolly Roger pinnace fire in the hole Barbary Coast knave snow.',
+  'Powder chocolate cake carrot cake macaroon sweet sweet marzipan cupcake chocolate.',
+  'Lemon drops cheesecake croissant cupcake. Jelly-o jujubes liquorice lemon drops jelly-o cheesecake.',
+  'Jelly beans lollipop chupa chups sugar plum sweet roll dragée topping lollipop sesame snaps.',
+  'Boursin cauliflower cheese pepper jack cheeseburger cut the cheese dolcelatte cheese and biscuits cauliflower cheese.',
+  "Who moved my cheese cheesecake pepper jack manchego stinking bishop cheeseburger when the cheese comes out everybody's happy babybel.",
+];
+
 
 // Function to generate random word
 const wordRand = () => words[Math.floor(Math.random() * words.length)];
+// Function to generate random sentences
+const textRand = () => sentences[Math.floor(Math.random() * sentences.length)];
 
 // Funtion to generate nav link words
 const navLinks = document.querySelector('.nav-links');
@@ -60,41 +73,23 @@ if (navWords.includes(word)) {
 }
 headerText.innerHTML = `${word}`;
 
+// About section in main header {
+const about = document.querySelector('#about');
+about.innerHTML = textRand();
+
 // Function to dynamically generate layouts
 const addRow = () => {
   const row = document.createElement('section');
-  row.classList.add('row');
-
   // Randomly generate number of cards
   const cardsNum = Math.floor(Math.random() * 3) + 1;
-  // Random justify content
-  if (cardsNum !== 1) {
-    const justify = Math.floor(Math.random() * 3);
-    switch (justify) {
-      case 0:
-        if (cardsNum === 2) {
-          row.classList.add('center');
-        }
-        row.classList.add('space-between');
-        break;
-      case 1:
-        row.classList.add('space-around');
-        break;
-      case 2:
-        row.classList.add('space-evenly');
-        break;
-      case 3:
-        row.classList.add('center');
-        break;
-      default:
-        row.classList.add('center');
-    }
-  }
+  row.classList.add('row', `cols-${cardsNum}`);
 
   // Change card class based on number of cards
   let cardClass;
   // Change size of image based on number of cards
   let cardSize;
+  // If 2 cards, decide if want pic text hybrid
+  const picHybrid = Math.floor(Math.random() * 2);
   switch (cardsNum) {
     case 1:
       cardClass = 'card-1';
@@ -102,7 +97,7 @@ const addRow = () => {
       break;
     case 2:
       cardClass = 'card-2';
-      cardSize = '/300/300';
+      cardSize = picHybrid ? '/400/300' : '/300/300';
       break;
     case 3:
       cardClass = 'card-3';
@@ -112,38 +107,81 @@ const addRow = () => {
       cardClass = 'card-1';
       cardSize = '/200/200';
   }
+
   // Randomly make images circles
   const circle = Math.floor(Math.random() * 2);
   // Randomly have borders around cards
   const border = Math.floor(Math.random() * 2);
+  // Randomly decide if card should have description
+  const isText = Math.floor(Math.random() * 2);
 
-  for (let i = 1; i <= cardsNum; i += 1) {
-    const card = document.createElement('div');
-    card.classList.add('card', cardClass);
+  if (cardsNum === 2 && picHybrid) {
+    // Add border
     if (border) {
-      card.classList.add('border');
+      row.classList.add('border');
     }
-
-    // Randomly choose an image placeholder site
+    // Pic half
+    const picHalf = document.createElement('div');
+    picHalf.classList.add('pic-half');
+    picHalf.classList.add('card', cardClass);
     const img = document.createElement('img');
     img.classList.add('card-img');
     const imgURL = imgSites[Math.floor(Math.random() * imgSites.length)] + cardSize;
     img.src = imgURL;
-    img.alt = 'card';
-    if (circle && cardsNum !== 1) {
-      img.classList.add('circle');
+    img.alt = 'pic';
+    picHalf.appendChild(img);
+    row.appendChild(picHalf);
+
+    // Text half
+    const textHalf = document.createElement('div');
+    textHalf.classList.add(cardClass, 'text-half');
+    const textTitle = document.createElement('div');
+    textTitle.classList.add('hybrid-title');
+    textTitle.innerHTML = wordRand();
+    const textDesc = document.createElement('div');
+    textDesc.classList.add('hybrid-text');
+    textDesc.innerHTML = textRand();
+    textHalf.appendChild(textTitle);
+    textHalf.appendChild(textDesc);
+    row.appendChild(textHalf);
+  } else {
+    for (let i = 1; i <= cardsNum; i += 1) {
+      const card = document.createElement('div');
+      card.classList.add('card', cardClass);
+      // Add border
+      if (border) {
+        card.classList.add('border');
+      }
+
+      // Randomly choose an image placeholder site
+      const img = document.createElement('img');
+      img.classList.add('card-img');
+      const imgURL = imgSites[Math.floor(Math.random() * imgSites.length)] + cardSize;
+      img.src = imgURL;
+      img.alt = 'card';
+      if (circle && cardsNum !== 1) {
+        img.classList.add('circle');
+      }
+
+      // Randomly choose a card title
+      const cardTitle = document.createElement('div');
+      const wordCard = wordRand();
+      cardTitle.classList.add('card-title');
+      cardTitle.innerHTML = `${wordCard}`;
+      card.appendChild(img);
+      card.appendChild(cardTitle);
+
+      // Randomly choose a card description
+      if (isText) {
+        const cardText = document.createElement('div');
+        cardText.classList.add('card-text');
+        cardText.innerHTML = textRand();
+        card.appendChild(cardText);
+      }
+
+      // Add to bottom of page
+      row.appendChild(card);
     }
-
-    // Randomly choose a card text description
-    const cardText = document.createElement('div');
-    const wordCard = wordRand();
-    cardText.classList.add('card-text');
-    cardText.innerHTML = `${wordCard}`;
-    card.appendChild(img);
-    card.appendChild(cardText);
-
-    // Add to bottom of page
-    row.appendChild(card);
   }
 
   const content = document.querySelector('#content-main');
